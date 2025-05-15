6fe13958d2690ccdb01f81c9bebfb9c382bfa79b
KarolSalvador <karolsalvador91@gmail.com>
2025-05-15 09:01:36 -0300

    transformando a lista de pokemons em uma lista de HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,40 @@ const limit = 10;
 /*url que será utilizada para acessar os dados, colocando o offset e limit de forma dinâmica */
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">Grass</li>
+                    <li class="type">Poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+
+            </div>
+        </li>
+    `
+}
+
 /*fetch retorna uma promise que é usada para lidar com assincronismo no JS. Uma Promise é usada para evitar que a página trave enquanto o navegador espera alguma tarefa demorada terminar (tipo buscar dados de uma API). Quando essa tarefa termina, a Promise avisa se deu certo (resolve) ou se deu ruim (reject)*/
+
+const pokemonList = document.getElementById("pokemonList")
+
 fetch(url)
     //caso a resposta(response) seja sucesso, você programa o que será feito com o then, nesse caso solicita o json da response
     .then((response) => response.json())
-    //tendo sucesso no requisição anterior, define o que será feito com o retorno com um novo then
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for(let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+
+           pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+    })
     //caso dê erro no carregamento, você trata com o catch
     .catch((error) => console.log(error))
     //caso deseje sinalizar, independente do sucesso ou do fracasso pode usar o finally
