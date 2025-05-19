a68a025340f1e2a31d3bdfd0b9be5d3138229f2b
KarolSalvador <karolsalvador91@gmail.com>
2025-05-19 10:06:15 -0300

    Adicionando os tipos de Pokémon dinamicamente

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,10 +3,10 @@ const pokeApi = {}
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.order
-    pokemon.name = pokeApi.name
+    pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [ type ] = types
+    const [type] = types
     pokemon.types = types
     pokemon.type = type
 
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
 
     /*url que será utilizada para acessar os dados, colocando o offset e limit de forma dinâmica */
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
