2b7d24cf996254d2a29c21dab5bd3f4a48633410
KarolSalvador <karolsalvador91@gmail.com>
2025-05-19 11:04:50 -0300

    criando mecanismo de paginacao, adicionado hover ao botao de paginacao, adicionado spinner enquanto novos itens do botao de paginacao carregam

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,51 @@
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 8;
+let offset = 0;
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(' ')}
-                </ol>
-
-                <img src="${pokemon.photo}">
-
-            </div>
-        </li>
-    `
+function showLoadingSpinner() {
+    loadingSpinner.classList.remove('hidden');
 }
+
+function hideLoadingSpinner() {
+    loadingSpinner.classList.add('hidden');
+}
+
 
 /*fetch retorna uma promise que é usada para lidar com assincronismo no JS. Uma Promise é usada para evitar que a página trave enquanto o navegador espera alguma tarefa demorada terminar (tipo buscar dados de uma API). Quando essa tarefa termina, a Promise avisa se deu certo (resolve) ou se deu ruim (reject)*/
 
-const pokemonList = document.getElementById("pokemonList")
+function loadPokemonItens(offset, limit) {
+    showLoadingSpinner();
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                  <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(' ')}
+                  </ol>
+
+                    <img src="${pokemon.photo}">
+
+                </div>
+            </li>
+        `).join(' ')
+        pokemonList.innerHTML += newHtml;
+        hideLoadingSpinner();
+    }).catch((error) => {
+        console.error("Erro ao carregar Pokemons:", error);
+        hideLoadingSpinner();
+    }) 
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
+
